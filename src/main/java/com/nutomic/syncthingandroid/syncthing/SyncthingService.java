150b613d84f1b2e782a418f418a5ce3ede13fa63
Felix Ableitner <me@nutomic.com>
2016-09-23 12:00:27 +0900

    Moved PRNGFixes to own package

--- src/main/java/com/nutomic/syncthingandroid/syncthing/SyncthingService.java
+++ src/main/java/com/nutomic/syncthingandroid/syncthing/SyncthingService.java
@@ -27,7 +27,7 @@ import com.nutomic.syncthingandroid.R;
 import com.nutomic.syncthingandroid.activities.MainActivity;
 import com.nutomic.syncthingandroid.util.ConfigXml;
 import com.nutomic.syncthingandroid.util.FolderObserver;
-import com.nutomic.syncthingandroid.util.PRNGFixes;
+import com.android.PRNGFixes;
 
 import java.io.File;
 import java.io.FileInputStream;
