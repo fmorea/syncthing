43c71cf6d9f09428ff9df81e6dd16365c0c12c95
Unrud <Unrud@users.noreply.github.com>
2017-07-18 05:33:20 +0200

    Don't access devices/folders when config is not loaded (fixes #876) (#924)

--- src/main/java/com/nutomic/syncthingandroid/fragments/DevicesFragment.java
+++ src/main/java/com/nutomic/syncthingandroid/fragments/DevicesFragment.java
@@ -108,7 +108,7 @@ public class DevicesFragment extends ListFragment implements SyncthingService.On
      */
     private void updateList() {
         SyncthingActivity activity = (SyncthingActivity) getActivity();
-        if (activity.getApi() == null || getView() == null || activity.isFinishing())
+        if (activity.getApi() == null || !activity.getApi().isConfigLoaded() || getView() == null || activity.isFinishing())
             return;
 
         if (mAdapter == null) {
